9deadd4ad8729bc66657ca66eba2f3140fc4b84c
Wen <wfwuestc@163.com>
2017-11-23 21:54:08 +0800

    change leancloud key

--- src/lib/leancloud.js
+++ src/lib/leancloud.js
@@ -1,7 +1,7 @@
 import AV from 'leancloud-storage'
 
-var APP_ID = 'dMCAfvAY0m9PAzthjX9xXWPH-gzGzoHsz';
-var APP_KEY = 'mIOLcTqescfST8fKTLjlBuyR';
+var APP_ID = 'XtBA9qy1k1W1Fd0yrYfvdXoS-gzGzoHsz';
+var APP_KEY = 'UOmizdTmGeyJqFWSK3itRgRo';
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
